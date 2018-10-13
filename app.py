4831663856696923b105d3df94e2825cf2b42731
Richard McMaster <40280868@napier.ac.uk>
2018-10-13 13:27:29 +0100

    Embed host and port details within app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-	return redirect('/index.html')
+	return ('Welcome')
 
 if __name__ == '__main__':
-	app.run(debug=True)
+	app.run(host="0.0.0.0",port=9157,debug=True)
